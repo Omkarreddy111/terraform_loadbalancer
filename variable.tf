9d4a575ef3b0d2646cdfedfe5f581192e2c71d0b
Omkarreddy111 <99641843+Omkarreddy111@users.noreply.github.com>
2023-06-13 11:54:15 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,32 @@
+variable "terraform lb" {
+    type = string
+  
+}
+
+variable "false" {
+    type = string
+  
+}
+
+variable "cloud application" {
+    type = string
+  
+}
+
+variable "cloud security_group" {
+    type = string
+
+  
+}
+
+variable "cloud subnet" {
+    type = string
+
+  
+}
+
+variable "terraform enable_deletion_protection" {
+    type = string
+  
+}
+
